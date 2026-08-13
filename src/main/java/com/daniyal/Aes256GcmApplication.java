f8ef47f902ab3856ef254d92f8a5c17eff1e3ab6
dani <daniyalajalloueian@gmail.com>
2026-08-13 11:57:15 +0330

    first commit

---
+++ src/main/java/com/daniyal/Aes256GcmApplication.java
@@ -0,0 +1,13 @@
+package com.daniyal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aes256GcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aes256GcmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniyal/Aes256GcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniyal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aes256GcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
