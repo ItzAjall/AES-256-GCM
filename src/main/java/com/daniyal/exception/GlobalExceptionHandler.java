3b897edd44e2b5911ca07a874ae5984687af0bbd
dani <daniyalajalloueian@gmail.com>
2026-08-13 12:50:05 +0330

    changed the error response from INTERNAL_SERVER_ERROR to BAD_REQUEST

--- src/main/java/com/daniyal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/daniyal/exception/GlobalExceptionHandler.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
